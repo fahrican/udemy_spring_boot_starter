3cfa0c53bd258ddcfd46c45943081d60c97039c3
Fahri MacBook <fahri-can@web.de>
2021-12-19 17:05:18 +0100

    Add repo annotation.

--- src/main/java/com/example/udemy_spring_boot_starter/repository/AddressRepository.java
+++ src/main/java/com/example/udemy_spring_boot_starter/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.example.udemy_spring_boot_starter.repository;
 
 import com.example.udemy_spring_boot_starter.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
--- src/main/java/com/example/udemy_spring_boot_starter/repository/SubjectRepository.java
+++ src/main/java/com/example/udemy_spring_boot_starter/repository/SubjectRepository.java
@@ -2,6 +2,8 @@ package com.example.udemy_spring_boot_starter.repository;
 
 import com.example.udemy_spring_boot_starter.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 }
